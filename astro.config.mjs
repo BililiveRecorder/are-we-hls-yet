9bfcf558b3d9d5a8d62ee13b91668c4fd7ac9a9c
genteure <genteure@gmail.com>
2023-10-02 16:31:11 +0800

    basic frontend template

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), mdx(), tailwind()]
+  integrations: [solidJs(), mdx(), tailwind(), compress()]
 });
