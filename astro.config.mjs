9785de578f17142e20a5af07266b45958f0c576e
genteure <genteure@gmail.com>
2023-10-02 16:18:05 +0800

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import solidJs from "@astrojs/solid-js";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solidJs(), mdx(), tailwind()]
+});
